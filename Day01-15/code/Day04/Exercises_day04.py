0fccc158df54cb847f463aec77dba17a400b5677
Zihang Wei <u6123487@anu.edu.au>
2019-12-10 16:15:17 +0800

    Update day 2 and 4 exercises.

---
+++ Day01-15/code/Day04/Exercises_day04.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*- 
+# Author: Zihang
+
+# 1. prime number
+# number = int(input('Input an integer: '))
+#
+# status = True
+# for trial_factor in range(2, number):
+#     if number % trial_factor == 0:
+#         status = False
+#         break
+# if not status:
+#     print('%d is not a prime number, it is divisible by %d.' % (number, trial_factor))
+# else:
+#     print('%d is a prime number.' % number)
+
+# 2. greatest common divisor & least common multiple
