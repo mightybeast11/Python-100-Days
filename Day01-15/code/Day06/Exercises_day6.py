d97123ff5db6ae5126d133dbf8b0a2f44eebcf78
Zihang Wei <u6123487@anu.edu.au>
2019-12-16 10:15:08 +0800

    Complete day 6 exercises.

--- Day01-15/code/Day06/Exercises_day6.py
+++ Day01-15/code/Day06/Exercises_day6.py
@@ -19,7 +19,7 @@ def lcm(x, y):
 # 2. palindrome
 
 
-def palindrome(x):
+def is_palindrome(x):
     x_str = str(x)
     pair = len(x_str) // 2
     for i in range(pair):
@@ -27,10 +27,39 @@ def palindrome(x):
             return False
     return True
 
+# 3. prime
+
+
+def is_prime(x):
+    for factor in range(2, x):
+        if x % factor == 0:
+            return False
+    return True if x != 1 else False
+
+
+# 4. unfixed parameters: add * in front of the parameter.
+
+
+def unfixed(*a):
+    sum = 0
+    for i in a:
+        sum += i
+    return sum
+
+
 
 if __name__ == '__main__':
     print(gcd(3, 6))
     print(lcm(6, 3))
-    print(palindrome(12321))
-    print(palindrome(12323))
-    print(palindrome(12345678999987654321))
+    print(is_palindrome(12321))
+    print(is_palindrome(12323))
+    print(is_palindrome(12345678999987654321))
+
+    num = int(input('Please input an interger: '))
+    if is_palindrome(num) and is_prime(num):
+        print('%d is a prime palindrome.' % num)
+    else:
+        print('%d is not a prime palindrome.' % num)
+
+    print(unfixed(1, 3, 5))
+    print(unfixed(1, 2))
